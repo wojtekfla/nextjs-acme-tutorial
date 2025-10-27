30397659006708ba62b1516c3eee1fa09e179405
Wojtek <wojtek.wfla@gmail.com>
2025-10-27 21:54:05 +0100

    suspense feature added

--- app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function DashboardPage() {
-  return (
-    <>
-      <main className="bg-blue-300 items-center text-2xl">
-        <div>Dashboard page</div>
-      </main>
-    </>
-  );
-}
